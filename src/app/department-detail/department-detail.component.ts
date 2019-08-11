7e90732ddc55c8679aa2cd9ff31905da541a4bdd
zhaoshuai <314253127@qq.com>
2019-08-11 21:45:45 +0800

    111

--- src/app/department-detail/department-detail.component.ts
+++ src/app/department-detail/department-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router, ParamMap  } from '@angular/router';
 
 @Component({
   selector: 'app-department-detail',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DepartmentDetailComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private activatedRoute: ActivatedRoute , private router: Router) { }
+public id;
   ngOnInit() {
+    // tslint:disable-next-line: radix
+    // this.id = parseInt(this.activatedRoute.snapshot.paramMap.get('id'));
+    // tslint:disable-next-line: radix
+    this.activatedRoute.paramMap.subscribe((params: ParamMap) => {  this.id = parseInt(params.get('id')); } );
   }
 
+
+  gotoPre() {
+    let pid = this.id - 1;
+    this.router.navigate(['/departments', pid]);
+  }
+
+  gotoNext() {
+    let nid = this.id + 1;
+    this.router.navigate(['/departments', nid]);
+  }
 }
